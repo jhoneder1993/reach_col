791957f0fe910b7bc6c0d512cb35a9768026970a
jhoneder1993 <jhoneder1993@gmail.com>
2022-03-23 10:55:23 -0500

    Se agrego el return para excel_separados pero falta generar mejor return

--- R/excel_separados.R
+++ R/excel_separados.R
@@ -20,8 +20,11 @@ excel_separados <- function(df, variable="organizacion"){
                                       paste("Result/Clean_data/individuals/BD_", nombres[[1]][i] ,".xlsx", sep = ""))
   }
 
-  #print avance
+  # print avance
   print("La bases de datos individuales 'Result/Clean_data/individuals/BD_..' se han guardado de manera correcta.")
   cat("\n")
+
+  # Generar una salida
+  return("Ok")
 }
 
