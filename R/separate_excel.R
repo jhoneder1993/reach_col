4141c1b9ab6953b3f48c875f066c3bb8986cb417
jhoneder1993 <jhoneder1993@gmail.com>
2022-04-06 15:09:48 -0500

    Se ajusto la funcion separate_excel

---
+++ R/separate_excel.R
@@ -0,0 +1,34 @@
+#' Separate Excel
+#'
+#' Genera salidas individuales segun la variable ingresada, ej. si la variable utilizada es
+#' organizacion genera salida segun las organizaciones que se tengan.
+#' los archivos se guardan en la ruta Result/Clean_data/individuals/BD_..
+#'
+#' @param df data frame a utilizar
+#' @param variable variable con la cual separar los archivos de excel
+#' @param eliminar conjunto de nombres de las variables a eliminar si hay datos personales
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+
+separate_excel <- function(df, variable="organizacion", borrar=c()){
+
+  # borrar variables
+  df <- df[ , !(names(df) %in% borrar)]
+
+  ### Guardar bases de datos segun ORG
+  nombres <- df %>% select(variable[1]) %>% unique()
+
+  for (i in 1:nrow(nombres)) {
+    filtro <- df %>% filter(df[[variable]] %in% nombres[[1]][i])
+    dir_filtro <- writexl::write_xlsx(list("Base de datos" = filtro),
+                                      paste("Result/Clean_data/individuals/BD_", nombres[[1]][i] ,".xlsx", sep = ""))
+  }
+
+  # Generar una salida
+  return("La bases de datos individuales 'Result/Clean_data/individuals/BD_..' se han guardado de manera correcta.")
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## README
 
-Repository para la limpieza de JMMI COL
+Paquete utilizado para hacer limpieza de datos de los JMMIs Colombia, con funciones adicionales para otros proyectos como kobo_to_r, time_check, incorporate_logs, find_outliers, find_other_responses.
